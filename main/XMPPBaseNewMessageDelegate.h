afa92437c33b42cb722ec81e8fc1fffdb7815cbc
Anthony Perritano <aperritano@gmail.com>
2012-09-30 15:07:17 -0500

    added a comment.

--- main/XMPPBaseNewMessageDelegate.h
+++ main/XMPPBaseNewMessageDelegate.h
@@ -1,6 +1,8 @@
 //
 //  NewXMPPMessageDelegate.h
 //
+//  Protocol for recieving new messages
+//
 //  Created by Anthony Perritano on 9/30/12.
 //  Copyright (c) 2012 Learning Technologies Group. All rights reserved.
 //
