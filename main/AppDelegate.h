cf0b3007546d2bb45f86beb74556b98c89ffec29
Anthony Perritano <aperritano@gmail.com>
2012-09-30 15:09:12 -0500

    added NewMessageDelegate.h

--- main/AppDelegate.h
+++ main/AppDelegate.h
@@ -5,6 +5,8 @@
 //  Created by Anthony Perritano on 9/14/12.
 //  Copyright (c) 2012 Learning Technologies Group. All rights reserved.
 //
+//
+
 
 #import <UIKit/UIKit.h>
 #import "XMPPFramework.h"
